26d2469871ac29b9f8b8161deaf029b71be4cca0
Alexey Golomedov <nkdhny@yandex-team.ru>
2018-04-03 14:28:10 +0300

    PF HW assignment

--- seminar01-bayesian_filters/measurer/map_measurer.py
+++ seminar01-bayesian_filters/measurer/map_measurer.py
@@ -4,21 +4,17 @@ import cv2
 class MapMeasurer(object):
     def __init__(self, keypoints_map):
         self.map_ = keypoints_map.position
-        self.kp_id_ = keypoints_map.kp_id
 
-    def measure(self, robot):
-        yaw = robot.state_[-1]
-        pos = robot.state_[:2]
-
-        hom_map = np.vstack(self.map_, np.ones(len(self.map_)))
-
+    def measure(self, yaw, pos):
+        
+        hom_map = np.hstack((self.map_, np.ones((self.map_.shape[0], 1))))
         robot_to_map = np.array((
             (np.cos(yaw), -np.sin(yaw), pos[0]),
             (np.sin(yaw),  np.cos(yaw), pos[1]),
             (          0,            0,      1)
         ))
 
-        map_in_robot = (np.linalg.inv(robot_to_map) * self.map_.T).T
+        map_in_robot = (np.linalg.inv(robot_to_map).dot(hom_map.T)).T
         map_in_robot = map_in_robot[:, :2]
 
-        return map_in_robot, self.kp_id_
+        return map_in_robot.ravel()
